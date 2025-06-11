70e3f0b84b59d157ab47077323bb7229b35eb566
Darwin Perdomo <ZeroBl@hotmail.com>
2025-06-11 15:52:14 -0400

    Points, UI, Text and prefabs

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHandler : MonoBehaviour
 {
     private Rigidbody rb;
+    private int points = 0;
+
     public float speed = 10f;
-    //
+    // public Text pointsText, winText;
+    public Text pointsText, winText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        points = 0;
+        setPointsText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -30,6 +38,17 @@ public class PlayerHandler : MonoBehaviour
         if (other.gameObject.CompareTag("Collectible"))
         {
             other.gameObject.SetActive(false);
+            points++;
+            setPointsText();
+        }
+    }
+
+    void setPointsText()
+    {
+        pointsText.text = "Points: " + points.ToString();
+        if (points >= 12)
+        {
+            winText.text = "You Win!!";
         }
     }
 }
